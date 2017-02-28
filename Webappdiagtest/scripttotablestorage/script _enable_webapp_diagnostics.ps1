f34ced45f17f71c2907fcac98ad26ad8fe09e0a3
sumangithub <ssuman.besu@gmail.com>
2017-03-01 00:11:30 +1100

    add script to write web app diagnostic log into table storage

---
+++ Webappdiagtest/scripttotablestorage/script _enable_webapp_diagnostics.ps1
@@ -0,0 +1,36 @@
+﻿Select-AzureRmSubscription -SubscriptionId da90b945-2d6b-4ff4-a0e4-bf541cf8265f
+
+
+$resourceGroupName = "Webappdiagtestrg"
+
+$tableName = "wadlogtable"
+
+$webappName = "Webappdiagtest"
+
+$webAppLogResource = Get-AzureRmResource -ResourceGroupName $resourceGroupName -ResourceType Microsoft.Web/sites/config -ResourceName $webappName/logs -ApiVersion 2015-08-01
+
+$propertyObject = $webAppLogResource.Properties
+       
+
+$tableSasUrl = $propertyObject.applicationLogs.azureTableStorage.sasUrl    
+$storageAccountName = Get-AzureRmStorageAccount  -ResourceGroupName  $resourceGroupName
+$storageAccountKey = Get-AzureRmStorageAccountKey -ResourceGroupName $resourceGroupName -Name $storageAccountName.StorageAccountName
+
+$context = New-AzureStorageContext -StorageAccountName $storageAccountName.StorageAccountName -StorageAccountKey $storageAccountKey.Value[0]
+
+$storageTable = Get-AzureStorageTable -Name $tableName -Context $context -ErrorAction SilentlyContinue
+if($storageTable -eq $null)
+{
+$storageTable = New-AzureStorageTable -Name $tableName -Context $context
+}
+if($tableSasUrl.Length -eq 0)
+{
+$tableSasUrl = New-AzureStorageTableSASToken -Name $storageTable.Name -Context $context -Permission rau -FullUri -ExpiryTime (Get-Date).AddDays(99999)
+}
+       
+$propertyObject.applicationLogs.azureTableStorage.level = "Information"           
+$propertyObject.applicationLogs.azureTableStorage.sasUrl = "$tableSasUrl"
+$propertyObject.failedRequestsTracing.enabled = $true
+$propertyObject.detailedErrorMessages.enabled = $true
+       
+Set-AzureRmResource -PropertyObject $propertyObject -ResourceGroupName $resourceGroupName -ResourceType Microsoft.Web/sites/config -ResourceName $webappName/logs -ApiVersion 2015-08-01 -Force
